e8de2192d702f9fba5c554dcd242f9badefba265
Bogdan Nechyporenko <bnechyporenko@bol.com>
2025-01-03 19:10:21 +0100

    config

--- cyan-chroma/astro.config.mjs
+++ cyan-chroma/astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), tailwind()],
+  adapter: vercel()
+});
